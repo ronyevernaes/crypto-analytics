feeee0d17cb0e2fa5b5eba30c1ef0df13c7dbd96
Ronye Vernaes <ronye.vernaes@fin3.tech>
2023-02-10 14:24:44 +0000

    theme config

---
+++ frontend/src/styles/Theme.ts
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/system";
+import '@fontsource/red-hat-display';
+import '@fontsource/red-hat-display/500.css';
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '"Red Hat Display"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+    ],
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Red Hat Display';
+          font-style: normal;
+          font-weight: 500;
+          font-size: '14px',
+        }
+      `,
+    },
+  },
+});
