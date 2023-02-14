38cf7792ce8dbea82066fdff161f42bc592a4f03
Ronye Vernaes <ronye.vernaes@fin3.tech>
2023-02-14 08:08:53 +0000

    configure react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { CssBaseline } from '@mui/material';
 
 import { Toolbar } from './components/Widgets';
 import { List } from './components/Widgets/List/List';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <CssBaseline />
       {/* TODO: Add 'header' for semantic tag and proper branding */}
       <Toolbar />
       <List />
-    </div>
+    </QueryClientProvider>
   );
 }
 
