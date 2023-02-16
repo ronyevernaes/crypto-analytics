2b157432cd36559f89369eaa90dd37bd5f24d671
Ronye Vernaes <ronye.vernaes@fin3.tech>
2023-02-16 00:52:43 +0000

    implementing pagination at the list

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -1,5 +1,16 @@
-import React, { ReactNode } from 'react';
-import { Table as MUITable, TableBody, TableContainer, TableHead } from '@mui/material';
+import React, { ReactNode, MouseEvent, useState, ChangeEvent } from 'react';
+import {
+  Box,
+  IconButton,
+  Table as MUITable,
+  TableBody,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow
+} from '@mui/material';
+import { LastPage, FirstPage, KeyboardArrowRight, KeyboardArrowLeft } from '@mui/icons-material';
 
 import { StyledTableRow, StyledTableHeaderRow, StyledTableCell, StyledTableHeaderCell } from './Table.styled';
 
@@ -18,6 +29,23 @@ interface TableProps<T> {
 }
 
 export const Table = <T,>({ id: tableId, data, columns, loading }: TableProps<T>) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (
+    _: MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 5));
+    setPage(0);
+  };
+
   return (
     <TableContainer>
       <MUITable>
@@ -29,7 +57,12 @@ export const Table = <T,>({ id: tableId, data, columns, loading }: TableProps<T>
 
         <TableBody>
           {loading}
-          {!loading && data && data.map((rowData: T, index) => (
+          {!loading && 
+            data && 
+            (rowsPerPage > 0
+              ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : data
+            ).map((rowData: T, index) => (
             <StyledTableRow key={`${tableId}-row-${index}`}>
               {columns.map(({ id, render }) => (
                 <StyledTableCell key={`${tableId}-row-${index}-${id as string}`}>
@@ -39,7 +72,87 @@ export const Table = <T,>({ id: tableId, data, columns, loading }: TableProps<T>
             </StyledTableRow>
           ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5]}
+              count={data?.length || 0}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </MUITable>
     </TableContainer >
   );
 };
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number,
+  ) => void;
+}
+
+function TablePaginationActions({ count, page, rowsPerPage, onPageChange }: TablePaginationActionsProps) {
+  const handleFirstPageButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        <FirstPage />
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        <KeyboardArrowLeft />
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        <KeyboardArrowRight />
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        <LastPage />
+      </IconButton>
+    </Box>
+  );
+}
